b126d14209af996e2165f4cf85e56871ab18ba82
Alexandre ARRIGHI <alexandre@MacBook-Pro-de-Alexandre-2.local>
2014-10-01 12:13:04 +0200

    update2

---
+++ Simple Sharing List/AAFFriendPickerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AAFBFriendPickerTableViewController.h
+//  Simple Sharing List
+//
+//  Created by Alexandre ARRIGHI on 29/09/2014.
+//  Copyright (c) 2014 Alexandre ARRIGHI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAFBFriendPickerTableViewController : UITableViewController
+
+@end
